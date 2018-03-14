3b18b2e36dca4bd5f5c46460b57b38aaf176cc67
eddyD45 <deleoneddy45@gmail.com>
2018-03-13 22:26:59 -0700

    union method fixed, comments for context

--- Assignment2Problem6.java
+++ Assignment2Problem6.java
@@ -8,12 +8,11 @@ public class Assignment2Problem6 {
 		int [] set1 = getSet();
 		System.out.println("You will now build set #2.");
 		int [] set2 = getSet();
+		
 		union(set1,set2);
 		intersect(set1, set2);
 		set2Complement(set2);
 		cartesianProduct(set1, set2);
-		
-
 		
 	}
 	
@@ -31,9 +30,9 @@ public class Assignment2Problem6 {
 		while (count != set.length) {
 			System.out.println("Please enter index " + count + " of your set: ");
 			temp = sc.nextInt();
-			if(temp == 0 && !zeroSet) {
-				zeroSet = true;
-			}
+			if(temp == 0 && !zeroSet) {														//arrays are initialized to zero by default, so adding 0 is an auto duplicate
+				zeroSet = true;																//setting a flag for the first zero set allows the user to add a zero
+			}																				//without the false positive due to array initialization
 			else {
 				for(int i = 0; i <set.length; i++) {
 					if (set[i] == temp) {
@@ -47,9 +46,9 @@ public class Assignment2Problem6 {
 				set[count] = temp;
 				count++;
 			}
-			dupe = false;
+			dupe = false; //reset dupe flag for next iteration
 		}
-		Arrays.sort(set);
+		Arrays.sort(set);	//sorted arrays behave more like sets, so they're easier to work with
 		return set;
 	}
 	
@@ -61,60 +60,86 @@ public class Assignment2Problem6 {
 
 			for (int i = 0; i < set1.length; i++) {
 				if (i == set1.length - 1) {
-					unionstring += Integer.toString(set1[i]);
+					unionstring += Integer.toString(set1[i]); //for the sake of clean output
 				}
 				else {
-					unionstring += Integer.toString(set1[i]) + ",";
+					unionstring += Integer.toString(set1[i]) + ","; 
 				}
 				
 			}
 			unionstring += "}";
 			
 		}
+		else {
 		
-		for(int i = 0; i < set1.length; i++) {
-			for (int j = 0; j < set2.length; j++) {
-				size++;
-			}
-		}
-		int [] intersectSet = new int[size];							//create intersectSet array based off how many common elements you find
-		
-		for(int i = 0; i < set1.length; i++) {						//go back and check what elements are the ones they have in common, add to intersectSet
-			for (int j = 0; j < set2.length; j++) {
-				if(set1[i] == set2[j]) {
-					intersectSet[count] = set1[i];
-					count++;
+			for(int i = 0; i < set1.length; i++) {
+				for (int j = 0; j < set2.length; j++) {
+					if(set1[i] == set2[j]) {
+						size++;
+					}
 				}
 			}
+			int [] intersectSet = new int[size];							//create intersectSet array based off how many common elements you find
+			
+			for(int i = 0; i < set1.length; i++) {						//go back and check what elements are the ones they have in common, add to intersectSet
+				for (int j = 0; j < set2.length; j++) {
+					if(set1[i] == set2[j]) {
+						intersectSet[count] = set1[i];
+						count++;
+					}
+				}
+			}
+																		//we have to make a set for the union, so we calculate the size by taking the difference between 
+			size = set1.length + set2.length - intersectSet.length;		//the total length of set1 + set2 and the intersect length
+			int [] unionSet = new int[size];
+			
+			int i = 0, j = 0;											//we have to iterate through both set1 and set2, then set a count = 0 to keep an index of unionSet
+			count = 0;
+			
+		
+			while(i < set1.length && j < set2.length) {					//let's compare sets first for intersects
+				if(set1[i] < set2[j]) {									//we have two different indicies, i and j for set 1 and set 2 respectively
+					unionSet[count] = set1[i];							//if the i value of set1 is still less than the j value of set2, lets let set1[i]
+					count++;											//be the next value to push into unionSet. Now we increment i and count so next
+					i++;												//iteration of this loop, we check the next index of set1 and the next append onto
+				}														//unionSet is done on the next index
+				else if (set2[j] < set1[i]) {							//-- this is the same logic as the above, but we find that set 2 is less than set 1
+					unionSet[count] = set2[j];
+					count++;
+					j++;
+				}
+				else {
+					unionSet[count] = set2[j];							//if neither is less than, they're equal. Let's choose a value from one set, then 
+					count++;											//add that to unionSet and increment both indicies
+					i++;
+					j++;
+				}
+			}
+			
+			while (i < set1.length) {									//if you reach this point, that means either one of set 1 or set 2 is out of bounds
+				unionSet[count] = set1[i];								//these 2 while loops continue appending to unionSet until the last values are accounted for
+				count++;
+				i++;
+			}
+			while(j < set2.length) {
+				unionSet[count] = set2[j];
+				count++;
+				j++;
+			}
+			
+			for  (i = 0; i < unionSet.length; i++) {
+				if (i == unionSet.length - 1) {
+					unionstring += Integer.toString(unionSet[i]); //for the sake of clean output
+				}
+				else {
+					unionstring += Integer.toString(unionSet[i]) + ",";
+				}
+				
+			}
+			unionstring += "}";
 		}
 		
-		size = (set1.length + set2.length - intersectSet.length );
-//		int [] unionSet = new int [size];
-		
-		
-		
-	
-//		for(int i = 0; i < unionSet.length; i++) {
-//			if(set1[set1.length - 1] < intersectSet[0]) {
-//				while(set1[count] < intersectSet[0]) {
-//					unionSet[i] = set1[count];
-//					count++;
-//				}
-//				count = 0;
-//				while(intersectSet[count] < set2[0]) {
-//					
-//				}
-//			}
-//			
-//		}
-		
-		
-		
-		
-		
-		
-		
-		
+
 		System.out.println("The union of sets 1 and 2 is " + unionstring);
 	}
 	
@@ -143,7 +168,7 @@ public class Assignment2Problem6 {
 		
 		for (int i = 0; i < intersectSet.length; i++) {
 			if (i == intersectSet.length - 1) {
-				intersectString += Integer.toString(intersectSet[i]);
+				intersectString += Integer.toString(intersectSet[i]);//for the sake of clean output
 			}
 			else {
 				intersectString += Integer.toString(intersectSet[i]) + ",";
@@ -179,7 +204,7 @@ public class Assignment2Problem6 {
 		
 		for (int i = 0; i < complementSet.length; i++) {
 			if (i == complementSet.length - 1) {
-				complementString += Integer.toString(complementSet[i]);
+				complementString += Integer.toString(complementSet[i]); //for the sake of clean output
 			}
 			else {
 				complementString += Integer.toString(complementSet[i]) + ",";
@@ -203,7 +228,6 @@ public class Assignment2Problem6 {
 			}
 		}
 		int [] intersectSet = new int[size];							//create intersectSet array based off how many common elements you find
-		System.out.println(intersectSet.length);
 		
 		for(int i = 0; i < set1.length; i++) {						//go back and check what elements are the ones they have in common, add to intersectSet
 			for (int j = 0; j < set2.length; j++) {
@@ -216,7 +240,7 @@ public class Assignment2Problem6 {
 		
 		for(int i = 0; i < intersectSet.length; i++) {
 			for(int j = 0; j < set1.length; j++) {
-				if (intersectSet[i] == intersectSet[intersectSet.length-1]&& set1[j] == set1[set1.length - 1]) {
+				if (intersectSet[i] == intersectSet[intersectSet.length-1]&& set1[j] == set1[set1.length - 1]) { //for the sake of clean output
 					productString += ("(" + intersectSet[i] + ", " + set1[j] + ")");
 				}
 				else {
